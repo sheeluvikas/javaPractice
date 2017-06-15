ab9289d8d52c6d2926b8449fb320a05728f2fac3
sheeluvikas <vikaskatiyar@live.com>
2017-06-15 20:13:32 +0530

    Changes for Serialization.

--- src/com/serialization/SerializationDemo.java
+++ src/com/serialization/SerializationDemo.java
@@ -10,16 +10,16 @@ public class SerializationDemo {
 
 	public static void main(String args[]) {
 		// Object serialization
-//		try (ObjectOutputStream objOStrm = new ObjectOutputStream(new FileOutputStream("serial"))) {
-//			MyClass object1 = new MyClass("Hello", -7, 2.7e10);
-//			System.out.println("object1: " + object1);
-//			ClassSuper classSuper = new ClassSuper();
-//			System.out.println(object1.superInt);
-//			objOStrm.writeObject(object1);
-//			objOStrm.writeObject(classSuper);
-//		} catch (IOException e) {
-//			System.out.println("Exception during serialization: " + e);
-//		}
+		try (ObjectOutputStream objOStrm = new ObjectOutputStream(new FileOutputStream("serial"))) {
+			MyClass object1 = new MyClass("Hello", -7, 2.7e10);
+			System.out.println("object1: " + object1);
+			ClassSuper classSuper = new ClassSuper();
+			System.out.println(object1.superInt);
+			objOStrm.writeObject(object1);
+			objOStrm.writeObject(classSuper);
+		} catch (IOException e) {
+			System.out.println("Exception during serialization: " + e);
+		}
 		// Object deserialization
 		try (ObjectInputStream objIStrm = new ObjectInputStream(new FileInputStream("serial"))) {
 			MyClass object2 = (MyClass) objIStrm.readObject();
